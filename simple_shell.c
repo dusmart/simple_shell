7d3f402a0a673f930cf215e8f3abcddce6a08140
dusmart <dusmart@qq.com>
2017-03-11 14:03:10 +0800

    be able to call history command using 'r x'

--- simple_shell.c
+++ simple_shell.c
@@ -9,14 +9,20 @@
 #define TRUE 1
 #define FALSE 0
 #define HISTORY_NUM 10
+// color
+#define NONE          "\033[m"
+#define COLOE_LIGHT_BLUE    "\033[1;34m"
+#define COLOR_LIGHT_CYAN    "\033[1;36m"
+#define COLOR_PURPLE        "\033[0;35m"
 
 
 char history[HISTORY_NUM][MAX_LINE];
 int history_index = 0;
 
-void setup(char inputBuffer[], int length, char *args[], int *background){
-    int i, start = -1, count = 0;
+int setup(char inputBuffer[], char *args[], int *background){
+    int i, start = -1, count = 0, length = strlen(inputBuffer);
     char tmp[MAX_LINE];
+    *background = FALSE;
     memcpy(tmp, inputBuffer, MAX_LINE);
     for(i=0; i<length; ++i){
         switch(inputBuffer[i]){
@@ -50,23 +56,71 @@ void setup(char inputBuffer[], int length, char *args[], int *background){
     args[count] = NULL;
     if(count != 0){
         memcpy(history[(history_index++)%10], tmp, MAX_LINE);
-        //printf("\n---history number = %d---\n",history_index);
+        return 0;
+    }
+    else return -1;
+}
+
+void excute(char *args[], const int background){
+    // fork a child process to do the command
+    pid_t pid = fork();
+    if(pid < 0){
+        // handle error
+        fprintf(stderr, COLOR_PURPLE"Fork failed!\n"NONE);
+        exit(-1);
+    }
+    else if(pid == 0){
+        // child process
+        if(execvp(args[0], args) == -1){
+            printf(COLOR_PURPLE"Warning: There is no such instruction.\n"NONE);
+        }
+        exit(0);
+    }
+    else{
+        //parent process
+        if(background == FALSE){
+            waitpid(pid, NULL, 0);
+        }
     }
 }
 
 void handle_SIGINT(int sig){
+    char inputBuffer[MAX_LINE];
+    char *args[MAX_LINE/2 + 1];
+    int length, background;
     int i = (history_index<10)?0:(history_index-10);
     printf("\n");
     for(;i<history_index;++i){
-        printf("%d: ", i);
+        printf("%d: ", i+1);
         printf("%s", history[i%10]);
         fflush(stdout);
     }
-    //exec_history_command();
-    char inputBuffer[MAX_LINE];
+    //excute history command
+    printf(COLOR_LIGHT_CYAN"HISTORY >> "NONE);
+    fflush(stdout);
     if((length = read(STDIN_FILENO, inputBuffer, MAX_LINE)) == 0) exit(0);
-
-
+    while(inputBuffer[0] != 'r' && (length==2 || length==4)){
+        printf(COLOR_PURPLE"Warning: You can only use 'r x' or 'r' format to call history commands.'x' is the first letter of that command.\n"NONE);
+        printf(COLOR_LIGHT_CYAN"HISTORY >> "NONE);
+        fflush(stdout);
+        if((length = read(STDIN_FILENO, inputBuffer, MAX_LINE)) == 0) exit(0);
+    }
+    if(length == 2){
+        printf("%s", history[(history_index-1)%10]);
+        strcpy(inputBuffer, history[(history_index-1)%10]);
+        setup(inputBuffer, args, &background);
+        excute(args, background);
+    }
+    else if(inputBuffer[1] == ' '){
+        for(i=history_index-1;i>=0&&i>=history_index-10;--i){
+            if(history[i%10][0] == inputBuffer[2]) break;
+        }
+        printf("%s", history[i%10]);
+        strcpy(inputBuffer, history[i%10]);
+        setup(inputBuffer, args, &background);
+        excute(args, background);
+    }
+    fflush(stdout);
 }
 
 int main(){
@@ -74,7 +128,6 @@ int main(){
     char inputBuffer[MAX_LINE];
     int length, background;
     char *args[MAX_LINE/2 + 1];
-    pid_t pid;
     // set up the signal handler
     struct sigaction handler;
     handler.sa_handler = handle_SIGINT;
@@ -82,32 +135,13 @@ int main(){
 
     // the main loop
     while(TRUE){
-        background = FALSE;
-        printf("COMMAND >> ");
+        printf(COLOE_LIGHT_BLUE"COMMAND >> "NONE);
         fflush(stdout);
         // read command
         if((length = read(STDIN_FILENO, inputBuffer, MAX_LINE)) == 0) exit(0);
         inputBuffer[length] = '\0';
         // parse the args in command and store right command to history
-        setup(inputBuffer, length, args, &background);
-        // fork a child process to do the command
-        pid = fork();
-        if(pid < 0){
-            // handle error
-            fprintf(stderr, "Fork failed!\n");
-            exit(-1);
-        }
-        else if(pid == 0){
-            // child process
-            execvp(args[0], args);
-            exit(0);
-        }
-        else{
-            //parent process
-            if(background == FALSE){
-                waitpid(pid, NULL, 0);
-            }
-        }
-
+        setup(inputBuffer, args, &background);
+        excute(args, background);
     }
 }
